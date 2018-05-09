76622a8622656fa2868c69e0e5300c28ace935a8
djrakita <rakita@cs.wisc.edu>
2018-05-09 11:04:45 -0500

    added more to start_here

--- src/start_here.py
+++ src/start_here.py
@@ -27,7 +27,8 @@ or negative experiences in using it.
 # Step 0: The RelaxedIK project should be included as a package within a catkin workspace
 #   for more info on this step, see http://wiki.ros.org/catkin/workspaces
 #   Make sure the package builds correctly (using catkin_make in the workspace root directory)
-#   and source is workspace such that the package is visible to the ROS environment
+#   and source the workspace such that the package is visible to the ROS environment
+#   (using source/devel setup.bash)
 ######################################################################################################
 
 
@@ -45,7 +46,164 @@ urdf_file_name = 'ur5.urdf'
 
 
 ######################################################################################################
-# Step 3: Please provide
+# Step 3: Please provide the names of the joints for all chains.
+#   This variable will be a list of lits, where each list specifies the names of joints in the particular
+#   chain, adhering to the naming scheme in the urdf supplied in step 2.
+#   example 1 shows what this would be for a multi-end effector robot, specifically to use the DRC-Hubo+ robots
+#   right and left arm with waist rotation (15 DOF total)
+#   ex1: [ ['WAIST', 'RIGHT_SHOULDER_PITCH', 'RIGHT_SHOULDER_ROLL', 'RIGHT_SHOULDER_YAW', 'RIGHT_ELBOW', 'RIGHT_WRIST_YAW',
+#               'RIGHT_WRIST_PITCH', 'RIGHT_WRIST_YAW_2'],
+#         ['WAIST', 'LEFT_SHOULDER_PITCH', 'LEFT_SHOULDER_ROLL', 'LEFT_SHOULDER_YAW', 'LEFT_ELBOW', 'LEFT_WRIST_YAW',
+#                'LEFT_WRIST_PITCH', 'LEFT_WRIST_YAW_2'] ]
+#   example 2 shows what this would be for a single-end effector robot, specifically using the UR5 robot
+#   ex2: [ ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'] ]
+joint_names = [  ]
+######################################################################################################
 
+
+######################################################################################################
+# Step 4: Please provide the order that you want joints to appear in the final returned joint configurations,
+#   using the names specified in step 3.  ALL JOINTS specified in step 3 should appear somewhere in
+#   this list.  If the same joint appears in separate chains in step 3, it should only appear once in
+#   the list here.  If you are using a single chain robot, feel free to use the exact same list
+#   that appears in step 3.
+#   example 1 shows one possible ordering for example 1 in Step 3.  Notice how in this example, the 'WAIST'
+#   joint only shows up once in the joint ordering list, even though it was a part of two separate subchains in
+#   Step 3.
+#   ex1: [ 'WAIST', 'RIGHT_SHOULDER_PITCH', 'RIGHT_SHOULDER_ROLL', 'RIGHT_SHOULDER_YAW', 'RIGHT_ELBOW', 'RIGHT_WRIST_YAW',
+#               'RIGHT_WRIST_PITCH', 'RIGHT_WRIST_YAW_2','LEFT_SHOULDER_PITCH', 'LEFT_SHOULDER_ROLL', 'LEFT_SHOULDER_YAW',
+#               'LEFT_ELBOW', 'LEFT_WRIST_YAW', 'LEFT_WRIST_PITCH', 'LEFT_WRIST_YAW_2' ]
+joint_ordering = [  ]
+######################################################################################################
+
+
+######################################################################################################
+# Step 5: Please provide the name(s) of all end-effector fixed joints.  These joints are usually labeled as
+#   "fixed" in the urdf, and specify the exact "grasping point" of the robot's hand.  If it appears
+#   that your urdf does not specify this ahead of time, please add it to the urdf.  Make sure to provide
+#   one end-effector joint name per chain (i.e., each chain will have its own end-effector).  The order of
+#   these joint names should correspond to the ordering of the chains specified in Step 3.
+#   For example 1, using the DRC-Hubo+ robot, we should specify two separate fixed joint names, one
+#   for the right hand and one for the left hand
+#   ex1: ee_fixed_joints = ['RIGHT_HAND', 'LEFT_HAND']
+#   For example 2, using the UR5, this is a single chain robot, so it will only have a single end-effector joint
+#   ex2: ee_fixed_joints = ['ee_fixed_joint']
+ee_fixed_joints = [  ]
+######################################################################################################
+
+
+######################################################################################################
+# Step 6: Please provide a function that takes in a vector corresponding to a robot configuration (x) and
+#   returns a sensor_msg.JointState message in ROS.  http://docs.ros.org/api/sensor_msgs/html/msg/JointState.html
+#   You do not have to include velocity or effort information, just the joint positions.
+#   The point of having you manually provide a function that does this is to account for situations
+#   where joints other than the ones included in Step 4 are part of a larger robot platform.  If you would
+#   rather not manually provide a function to do this, just leave it how it is by default (returning None), and
+#   we will provide a simple function that does this automatically.  But note, if you decide to do this, there may be
+#   errors when viewing things in rviz as only the default joints listed in Step 4 will be visible, while
+#   the other joints in the urdf are not published to.
+#
+# example function for the UR5:
+#
+# from sensor_msgs.msg import JointState
+# def joint_state_define(x):
+#    js = JointState()
+#    js.name = joint_names[0]
+#    js.position = tuple(x)
+#    return js
+#
+# example function for the DRC-Hubo+
+#
+# from sensor_msgs.msg import JointState
+# def joint_state_define(x):
+# js.name = ['LEFT_SHOULDER_PITCH', 'LEFT_SHOULDER_ROLL', 'LEFT_SHOULDER_YAW', 'LEFT_ELBOW', 'LEFT_WRIST_YAW',
+#                    'LEFT_WRIST_PITCH', 'LWFT', 'LEFT_WRIST_YAW_2', 'LHAND_a1', 'LHAND_a2',
+#                   'LHAND_a3', 'LHAND_b1', 'LHAND_b2', 'LHAND_b3', 'LHAND_c1',
+#                   'LHAND_c2', 'LHAND_c3', 'RIGHT_SHOULDER_PITCH', 'RIGHT_SHOULDER_ROLL', 'RIGHT_SHOULDER_YAW',
+#                   'RIGHT_ELBOW', 'RIGHT_WRIST_YAW', 'RIGHT_WRIST_PITCH', 'RWFT', 'RIGHT_WRIST_YAW_2',
+#                   'RHAND_a1', 'RHAND_a2', 'RHAND_a3', 'RHAND_b1', 'RHAND_b2',
+#                   'RHAND_b3', 'RHAND_c1', 'RHAND_c2', 'RHAND_c3', 'WAIST',
+#                   'LEFT_HIP_YAW', 'LEFT_HIP_ROLL', 'LEFT_HIP_PITCH', 'LEFT_KNEE', 'LEFT_ANKLE_PITCH',
+#                   'LEFT_ANKLE_ROLL', 'LAFT', 'LFUNI', 'LFWH', 'LEFT_WHEEL',
+#                   'RIGHT_HIP_YAW', 'RIGHT_HIP_ROLL', 'RIGHT_HIP_PITCH', 'RIGHT_KNEE', 'RIGHT_ANKLE_PITCH',
+#                   'RIGHT_ANKLE_ROLL', 'RAFT', 'RFUNI','RFWH', 'RIGHT_WHEEL',
+#                   'NECK_PITCH_1', 'NECK_YAW', 'NECK_PITCH_2', 'NECK_ROLL']
+#
+#        js.position = 59*[0]
+#        js.position[34] = x[0]
+#        js.position[17] = x[1]
+#        js.position[18] = x[2]
+#        js.position[19] = x[3]
+#        js.position[20] = x[4]
+#        js.position[21] = x[5]
+#        js.position[22] = x[6]
+#        js.position[24] = x[7]
+#        js.position[0] =  x[8]
+#        js.position[1] =  x[9]
+#        js.position[2] =  x[10]
+#        js.position[3] =  x[11]
+#        js.position[4] =  x[12]
+#        js.position[5] =  x[13]
+#        js.position[7] =  x[14]
+#        js.position = tuple(js.position)
+#        return js
+#
+#
+
+# TODO: fill out this function, or leave it how it is for the default option
+from sensor_msgs.msg import JointState
+def joint_state_define(x):
+    return None
+
+######################################################################################################
+
+
+######################################################################################################
+# Step 7: We will now set up collision information.  RelaxedIK avoids self-collisions by
+#   first receiving a potential function for "how close" it is to a collision state,
+#   then learning that potential function using a neural network such that it can be quickly run
+#   in the context of a real-time optimization.  RelaxedIK uses fcl (flexible
+#   collision library) to do distance checking between links and any environment objects.  Collision objects
+#   will automatically envelope the robot's links based on its geometry defined in the urdf, but you
+#   must supply a few other pieces of information for collision avoidance to work well.
+#
+#   To start, duplicate the collision_example.yaml file found in the RelaxedIK/Config directory, and rename it
+#   The renamed file should retain its yaml file extension and should also be in the RelaxedIK/Config
+#   directory.
+#
+#   Now, we'll work in this new file and make some adjustments to tailer the collision information to your robot platform.
+#   You can first set the desired radius for the robot's links.  The default option of 0.05 has been
+#   found to work well in most cases, but feel free to change that however you like.
+#
+#   Next, the robot needs a set of collision-free "sample states" so that it can learn what is close to a collision state and what is not.
+#   These sample states help the robot decide the difference between a configuration that is close to collision state and
+#   a configuration where two links are natively and safely close together.  THIS STEP IS VERY IMPORTANT FOR THE NEURAL
+#   NETWORK TO LEARN A GOOD COLLISION FUNCTION.  A set of 5 - 10 configurations where the robot is not in collision has been
+#   seen to work well, but more will always be better.  Add these collision-free sample states as lists next to the samples_states
+#   field in your yaml file, as seen in the collision_example.yaml file.
+#
+#   The next fields in the yaml file (boxes, spheres, ellipsoids, capsules, cylinders) are used to specify additional
+#   static objects around the environment that the robot should avoid.
+#   These fields all follow a similar pattern:
+#   1. Provide a name for the object under "name"
+#   2. Provide parameters for the respective shape
+#       - for boxes: [x_scale, y_scale, z_scale]
+#       - for spheres: radius    <---- note that this isn't a list, just a single value
+#       - for ellipsoids: [x_scale, y_scale, z_scale]
+#       - for capsules: [radius, height_along_z_axis]
+#       - for cylinders: [radius, height_along_z_axis]
+#   3. Provide the coordinate frame that this this collision object should be represented in.  If it is static with respect
+#       to the environment, this should be set to 0.  If, however, this should be coupled with the robot's end effector, for example
+#       this should be set to the joint index corresponding to the robot's end effector.
+#   4. Provide the rotation (in [rx, ry, rz] euler angles) that the object should be rotated, with respect to the coordinate
+#       frame specified in 3
+#   5. Provide the translation (in [x,y,z]) that the object should be translated, with respect to the coordinate frame
+#       specified in 3
+#
+#   REMEMBER: The robot's links are AUTOMATICALLY added, so no need to add these into the yaml file.  However, this
+#       likely won't include the robot's end effector since its geometry is often not included in the urdf, so a common
+#       strategy is to tightly envelope your end effector using an ellipsoid or box object in the yaml file
+#       so collisions with the end effector are avoided as best as possible.
+###########################################################################################################
 
 
