27d3b3ede96c41667e1d81145497bd96680bbfb2
djrakita <rakita@cs.wisc.edu>
2018-05-09 23:09:06 -0500

    set up sample

--- src/sample.py
+++ src/sample.py
@@ -3,26 +3,25 @@
 author: Danny Rakita
 website: http://pages.cs.wisc.edu/~rakita/
 email: rakita@cs.wisc.edu
-last update: 5/8/18
-
-PLEASE DO NOT CHANGE CODE IN THIS FILE.  IF TRYING TO SET UP RELAXEDIK, PLEASE REFER TO start_here.py INSTEAD
-AND FOLLOW THE STEP-BY-STEP INSTRUCTIONS THERE.  Thanks!
+last update: 5/9/18
 '''
 ######################################################################################################
 
-
 from start_here import urdf_file_name, joint_names, joint_ordering, ee_fixed_joints, starting_config, \
     joint_state_define, collision_file_name, fixed_frame
+from RelaxedIK.relaxedIK import RelaxedIK
 from RelaxedIK.GROOVE_RelaxedIK.relaxedIK_vars import RelaxedIK_vars
 from sensor_msgs.msg import JointState
 import rospy
 import roslaunch
 import os
 import tf
+import math
 
 
 if __name__ == '__main__':
-    rospy.init_node('collision_viewer')
+    # Don't change this code####################################################################################
+    rospy.init_node('sample_node')
 
     urdf_file = open(os.path.dirname(__file__) + '/urdfs/' + urdf_file_name, 'r')
     urdf_string = urdf_file.read()
@@ -30,47 +29,53 @@ if __name__ == '__main__':
     js_pub = rospy.Publisher('joint_states',JointState,queue_size=5)
     tf_pub = tf.TransformBroadcaster()
 
-    rospy.sleep(0.2)
+    rospy.sleep(0.3)
 
+    ####################################################################################################################
+
+
+
+    # Only change this if you want to extend/ adapt the solver to have more functionality ##############################
+    vars = RelaxedIK_vars('relaxedIK',os.path.dirname(__file__) + '/urdfs/' + urdf_file_name,joint_names,ee_fixed_joints,
+                          joint_ordering,init_state=starting_config, collision_file=collision_file_name)
+    relaxedIK = RelaxedIK(vars)
+    ####################################################################################################################
+
+
+
+    # Don't change this code ###########################################################################################
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
     launch_path = os.path.dirname(__file__) + '/../launch/robot_state_pub.launch'
     launch = roslaunch.parent.ROSLaunchParent(uuid, [launch_path])
     launch.start()
+    ####################################################################################################################
 
-    vars = RelaxedIK_vars('relaxedIK',os.path.dirname(__file__) + '/urdfs/' + urdf_file_name,joint_names,ee_fixed_joints,
-                          joint_ordering,init_state=starting_config, collision_file=collision_file_name, pre_config=True)
 
-    sample_states = vars.collision_graph.sample_states
-    idx = 0
-    rate = rospy.Rate(0.4)
+    rospy.sleep(1.0)
+
+    counter = 0.0
+    stride = 0.02
     while not rospy.is_shutdown():
-        if idx >= len(sample_states): idx = 0
+        c = math.cos(counter)
+        s = 0.1
+        xopt = relaxedIK.solve([[s*c,0,0]],[[1,0,0,0]])
 
-        state = sample_states[idx]
-        cg = vars.collision_graph
-        robot = vars.robot
-        frames = robot.getFrames(state)
-        cg.get_collision_score(frames)
-        cg.c.draw_all()
-
-        js = joint_state_define(state)
+        js = joint_state_define(xopt)
         if js == None:
             js = JointState()
             js.name = joint_ordering
-            for x in state:
+            for x in xopt:
                 js.position.append(x)
         now = rospy.Time.now()
         js.header.stamp.secs = now.secs
         js.header.stamp.nsecs = now.nsecs
         js_pub.publish(js)
 
-        tf_pub.sendTransform((0,0,0),
+        tf_pub.sendTransform((0, 0, 0),
                              tf.transformations.quaternion_from_euler(0, 0, 0),
                              rospy.Time.now(),
                              fixed_frame,
                              'common_world')
 
-        idx += 1
-
-        rate.sleep()
+        counter += stride
