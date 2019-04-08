53524aca436d3b56365903f0b08276649c021c01
djrakita <djrakita@gmail.com>
2019-04-08 15:37:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['relaxed_ik'],
+    packages=['RelaxedIK'],
     scripts=[''],
     package_dir={'': 'src'}
 )
